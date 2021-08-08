a24b26737ebcf72eb5277bbbcec1d5c60a9645e2
john <buildkdom@gmail.com>
2021-08-08 15:33:36 +0900

    [CodeUp] commit 2021.8.8

--- algorithm/codeup/class3000/3015-2.py
+++ algorithm/codeup/class3000/3015-2.py
@@ -19,12 +19,15 @@ people, max = int(people), int(max)
 
 scoreList = []
 
+# 입력되는 데이터를 받아오고, list 에 저장하는 과정
 for i in range(people):
     name, score = input().split()
     score = int(score)
-    scoreList.append([name, score])
+    scoreList.append((name, score))
 
+# 정렬~
 sortedScoreList = sorted(scoreList, key=lambda x: x[1], reverse=True)
-# print(sortedScoreList)
+
+#
 for j in range(max):
     print(sortedScoreList[j][0])
