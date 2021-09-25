eb5c2d105272d06028d657a73dfe98f2406ca89b
kkuma.coding <kkuma.coding@gmail.com>
2021-09-25 15:52:17 +0900

    [Codeup] 1535 without function usage

--- algorithm/codeup/class1000/1535.py
+++ algorithm/codeup/class1000/1535.py
@@ -5,12 +5,8 @@ https://codeup.kr/problem.php?id=1535
 1 3 2 1 3
 """
 
-import math
-import sys
-
-print(math.inf)
-print(float('-inf'))
-
-#
-# pos = 0
-# max =
+size = int(input())
+val = list(map(int, input().split()))
+if size > 0:
+    max_val = sorted(val, reverse=True)[0]
+    print(val.index(max_val)+1)
