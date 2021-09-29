f264195a6f1d97646dac21f5ddf4199316c02fe7
kkuma.coding <kkuma.coding@gmail.com>
2021-09-29 20:02:30 +0900

    [jungol] 701

---
+++ algorithm/jungol/701.py
@@ -0,0 +1,12 @@
+"""
+http://jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=4078&sca=py
+
+print 문장 세 개를 사용하여 다음과 같이 출력되는 프로그램을 작성하라.
+
+Score:
+100
+GREAT!
+"""
+print("Score:")
+print("100")
+print("GREAT!")
